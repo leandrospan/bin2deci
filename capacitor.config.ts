64173a3d1ba62e823271a71e929ec922659e8bd7
leandrospan <leandrospan@gmai.com>
2024-05-07 17:08:06 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Bin2Dec',
+  webDir: 'www'
+};
+
+export default config;
