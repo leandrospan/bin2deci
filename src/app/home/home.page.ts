caddb747d618191bcd3f292434689682baf90e4d
leandrospan <leandrospan@gmai.com>
2024-05-07 21:46:01 -0300

    Primeira Subida

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  binario: string = '';
+  decimal: number | null = null;
+  erro: string = '';
+
+
   constructor() {}
+
+  public converter() {
+    if (!this.ehBinario(this.binario)) {
+      this.erro = 'Por favor, digite um número binário válido (0 ou 1).';
+      this.decimal = null;
+      return;
+    }
+
+    this.decimal = parseInt(this.binario, 2);
+    this.erro = '';
+  }
+
+  ehBinario(b: string): boolean {
+    for (let i = 0; i < b.length; i++) {
+      if (b[i] !== '0' && b[i] !== '1') {
+        return false;
+      }
+    }
+    return true;
+  }
 
 }
